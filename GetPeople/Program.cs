8018e6ae736d863b6acb5d183218465a01c849bf
Avi Dgany <ahd_one@hotmail.com>
2017-08-21 03:30:39 +1000

    Add basic error handling to main

--- GetPeople/Program.cs
+++ GetPeople/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 
 namespace GetPeople
 {
@@ -8,11 +11,22 @@ namespace GetPeople
     {
         static void Main(string[] args)
         {
-            var sourceUrl = args.FirstOrDefault() ?? "http://agl-developer-test.azurewebsites.net/people.json";
-            var sourceContent = HttpHelper.GetContentFromUrl(sourceUrl);
-            var catList = CatListHelper.GetCatsByOwnerGender(sourceContent);
-            RenderingHelper.RenderList(catList, Console.Out);
+            try
+            {
+                var sourceUrl = args.FirstOrDefault() ?? "http://agl-developer-test.azurewebsites.net/people.json";
+                var sourceContent = HttpHelper.GetContentFromUrl(sourceUrl);
+                var catList = CatListHelper.GetCatsByOwnerGender(sourceContent);
+                RenderingHelper.RenderList(catList, Console.Out);
+            }
+            catch (Exception e)
+            {
+                var exceptions = (e as AggregateException)?.InnerExceptions ?? new [] {e}.AsEnumerable();
+                Console.Error.WriteLine("Execution failed due to the following errors:");
+                foreach (var ex in exceptions)
+                {
+                    Console.Error.WriteLine($"{ex.GetType().FullName}: {ex.Message}");
+                }
+            }
         }
-
     }
 }
